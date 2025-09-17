a185040127fa2c37ad3bc3dc59523a80a17e88e5
PhamNhatAnh2003 <anhpham090423@gmail.com>
2025-09-17 09:24:19 +0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+"Sửa lại đường dẫn tương đối hay tuyệt đối dưới đây trong 2 file:(preprocessor.py và routes.py) theo đúng cấu trúc thư mục của bạn.",
+"C:\Users\admin\Desktop\Used_cars_prediction-main\data\raw\raw.csv"
+"C:\Users\admin\Desktop\Used_cars_prediction-main\src\models"
